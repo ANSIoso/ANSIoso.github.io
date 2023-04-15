24a8030bcf09a75a36b7c3d845e619f73da1ab5f
Antonio Sisinni <92176296+ANSIoso@users.noreply.github.com>
2023-04-15 16:32:29 +0200

    javascript function test

--- calcoloAffinitaMM/logic.js
+++ calcoloAffinitaMM/logic.js
@@ -1,5 +1,12 @@
-var a = "akjdn";
+function submit_function() {
+    var name_1 = document.getElementById("name1").value;
+    var name_2 = document.getElementById("name2").value;
 
-function myFunction() {
-    document.getElementById("name1").;
+    var names_string = name_1 + " " + name_2;
+
+    calculate_letter_occurrance(names_string);
+}
+
+function calculate_letter_occurrance(names_string){
+    document.getElementById("out").innerHTML = names_string;
 }
